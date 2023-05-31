f7ea42dfe9822face82a2ad7a97ab1f38308ba05
SRINU <vasu_anand01@yahoo.co.in>
2023-05-31 16:46:44 +0530

    colour print

--- 02.print.sh
+++ 02.print.sh
@@ -1,2 +1,9 @@
 #To print a message we are using echo
 echo hello world
+#print a colour
+echo -e "\e[31m this is red colour \e[0m"
+echo -e "\e[32m this is green colour \e[0m"
+echo -e "\e[33m this is yellow colour \e[0m"
+echo -e "\e[34m this is blue colour \e[0m"
+echo -e "\e[35m this is megenta colour \e[0m"
+echo -e "\e[36m this is cyan colour \e[0m"
